ed40c3d7ec2c19ac40620262cc6a067c15c86b4e
Alexandre Alves <alexandre@kenzie.com.br>
2023-06-07 11:52:33 -0300

    feat: ajuste do retorno do serializer de album. Campo user retornou algumas informaçẽos ao invés de somente o id

--- albums/serializers.py
+++ albums/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
-
 from .models import Album
+from users.serializers import UserSerializer
 
 
 class AlbumSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
     year = serializers.IntegerField()
-    user_id = serializers.IntegerField(read_only=True)
+    user = UserSerializer(read_only=True)
 
     def create(self, validated_data):
         return Album.objects.create(**validated_data)
